22e9c82eb35308ff8ba90c978df686945e29934b
makso-87 <makso16108@gmail.com>
2026-01-07 15:10:14 +0300

    DEV-12 Исправлен баг с датой окончания - когда дата кончания была не задана, то приходил null и отображалаось дата 01.01.1970.

--- src/utils/calendar/getFormattedDate.ts
+++ src/utils/calendar/getFormattedDate.ts
@@ -1,6 +1,10 @@
 import { INVALID_DATE_TEXT } from '../../constants';
 
 export const getFormattedDate = (dateString: string) => {
-    const formattedDate = new Date(dateString).toLocaleDateString();
-    return formattedDate === INVALID_DATE_TEXT ? '' : formattedDate;
+    if (dateString) {
+        const formattedDate = new Date(dateString).toLocaleDateString();
+        return formattedDate === INVALID_DATE_TEXT ? '' : formattedDate;
+    }
+
+    return '';
 };
