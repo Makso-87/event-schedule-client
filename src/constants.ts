17ec70afde7d53f54818b187c5becb5d9dc835aa
makso-87 <makso16108@gmail.com>
2026-04-21 12:12:45 +0300

    DEV-20 Удалены неиспользуемые константы

--- src/constants.ts
+++ src/constants.ts
@@ -1,12 +1,8 @@
 export const LAST_MONTH_NUMBER = 11;
 export const FIRST_MONTH_NUMBER = 0;
-export const FIRST_MONTH_DAY = 1;
-export const LAST_ARRAY_ELEMENT_INDEX = -1;
-export const START_INDEX = 0;
-export const DATES_ARRAY_LOOP_GUARD_INDEX = 7;
-export const PREVIOUS_MONTH_START_DATE_COEFFICIENT = 2;
+
 export const SEVEN_DAY_OF_MONTH = 7;
-export const LONG_DASH = '&mdash;';
+
 export const INVALID_DATE_TEXT = 'Invalid Date';
 export const NEAREST_EVENTS_TITLE_TEXT = 'Ближайшие события';
 export const NEAREST_EVENT_TITLE_TEXT = 'Ближайшее событие';
